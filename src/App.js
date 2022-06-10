94ad50fdab200e34b5f9d4622feddae9b72a2564
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-06-10 16:36:26 +0000

    completed all functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+//get all unique categories adn adding all as part of the categories
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  //function to get all the available categories
+  const filterMenu = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    //function to get the menus based on the category button clicked
+    const filteredItems = items.filter((item) => item.category === category);
+    setMenuItems(filteredItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterMenu} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
